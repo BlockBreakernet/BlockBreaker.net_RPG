ba187f3f3594ebec471c7533585f0b79154b958e
Blackagent <ja7n7ne@gmx.de>
2015-03-30 18:59:11 +0200

    Teamwork

--- src/net/blockbreaker/rpg/api/mysql/MySQLManagementMethods.java
+++ src/net/blockbreaker/rpg/api/mysql/MySQLManagementMethods.java
@@ -1,12 +1,20 @@
 package net.blockbreaker.rpg.api.mysql;
 
+import java.sql.ResultSet;
+
 /**
  * Created by Lukas on 26.03.2015.
  */
 public class MySQLManagementMethods {
 
     public static void createTableIfNotExists() {
-        MySQL.update("CREATE TABLE IF NOT EXISTS Player (UUID VARCHAR(100), Player VARCHAR(100), Coins INTEGER)"); // TODO: Tabelle mit n?tigen Spalten best?cken
+        MySQL.update("CREATE TABLE IF NOT EXISTS LevelCoins (Playername VARCHAR(100), UUID VARCHAR(100), Ep INTEGER, Coins INTEGER)"); // TODO: Tabelle mit notigen Spalten bestucken
+        MySQL.update("CREATE TABLE IF NOT EXISTS ??????????? (Playername VARCHAR(100), UUID VARCHAR(100), ?????????????????????????)");
+    }
+
+    public static void getEP(String uuid) {
+        ResultSet ep = MySQL.getResult("SELECT Ep FROM LevelCoins WHERE UUID='"+uuid+"'");
+        return
     }
 
 }
