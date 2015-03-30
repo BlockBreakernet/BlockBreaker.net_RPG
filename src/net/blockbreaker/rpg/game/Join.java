f3dfe523201815b62ee38c6646d121578c7f9f03
Blackagent <ja7n7ne@gmx.de>
2015-03-30 21:34:23 +0200

    Teamwork

--- src/net/blockbreaker/rpg/game/Join.java
+++ src/net/blockbreaker/rpg/game/Join.java
@@ -3,16 +3,12 @@ package net.blockbreaker.rpg.game;
 import net.blockbreaker.rpg.api.coins.Coins;
 import net.blockbreaker.rpg.api.ep.Ep;
 import net.blockbreaker.rpg.api.mysql.MySQLManagementMethods;
+import net.blockbreaker.rpg.game.inventory.Items;
 import net.blockbreaker.rpg.system.Main;
-
-import org.bukkit.Material;
-
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-
-import org.bukkit.inventory.ItemStack;
 
 /**
  * Created by Lukas on 30.03.2015.
@@ -30,26 +26,14 @@ public class Join implements Listener {
     public void onJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
 
+
         MySQLManagementMethods.createData(p);
 
-        p.sendMessage("Deine EP: " + String.valueOf(Ep.getEP(p)));
-        Ep.setEp(p, 20);
-        p.sendMessage("Deine EP: " + String.valueOf(Ep.getEP(p)));
-        Ep.addEp(p, 1);
-        p.sendMessage("Deine EP: " + String.valueOf(Ep.getEP(p)));
-
-        p.sendMessage("Deine Coins: " + String.valueOf(Coins.getCoins(p)));
-        Coins.setCoins(p, 20);
-        p.sendMessage("Deine Coins: " + String.valueOf(Coins.getCoins(p)));
-        Coins.addCoins(p, 1);
-        p.sendMessage("Deine Coins: " + String.valueOf(Coins.getCoins(p)));
+        Items.getCoinItem(p.getName().toString());
 
         if(MySQLManagementMethods.isInDataBase(p) == true) {
             p.sendMessage("Boss in Datenbank");
         }
-
-        ItemStack coins = new ItemStack(Material.GOLD_INGOT, 100);
-
     }
 
 }
---
+++ src/net/blockbreaker/rpg/game/inventory/Items.java
@@ -0,0 +1,12 @@
+package net.blockbreaker.rpg.game.inventory;
+
+/**
+ * Created by Janne on 30.03.2015.
+ */
+public class Items {
+
+    public static void getCoinItem(String name) {
+
+    }
+
+}
